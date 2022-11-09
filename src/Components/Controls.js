8b27f5955c26bbb4d4e238ed27def65d0115e36e
nick <nicholasjpenaranda@gmail.com>
2022-11-08 20:51:22 -0600

    Add fullscreen theater mode and volume functionality

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,5 +1,13 @@
 import { ReactComponent as Play } from "../SVGs/play.svg";
 import { ReactComponent as Pause } from "../SVGs/pause.svg";
+import { ReactComponent as MiniPlayer } from "../SVGs/miniPlayer.svg";
+import { ReactComponent as Theater } from "../SVGs/theater.svg";
+import { ReactComponent as ExitTheater } from "../SVGs/exitTheater.svg";
+import { ReactComponent as Fullscreen } from "../SVGs/fullscreen.svg";
+import { ReactComponent as ExitFullscreen } from "../SVGs/exitFullscreen.svg";
+import { ReactComponent as HighVolume } from "../SVGs/highVolume.svg";
+import { ReactComponent as LowVolume } from "../SVGs/lowVolume.svg";
+import { ReactComponent as Mute } from "../SVGs/mute.svg";
 import { useEffect, useRef } from "react";
 
 function Controls({
@@ -9,13 +17,27 @@ function Controls({
   setPlayBool,
   videoDuration,
   currentTimeline,
+  theaterBool,
+  setTheaterBool,
+  fullscreenBool,
+  setFullscreenBool,
+  muteBool,
+  setMuteBool,
+  highVolumeBool,
+  setHighVolumeBool,
+  volumeValue,
+  setVolumeValue,
+  volumeCheck,
 }) {
   const timelineRef = useRef(null);
 
   useEffect(() => {
     const timelineWidth = currentTimeline / videoDuration;
     timelineRef.current.style.width = timelineWidth * 100 + "%";
-  }, [currentTimeline, videoDuration]);
+    volumeCheck(false);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTimeline, videoDuration, volumeValue]);
   return (
     <div className="controls">
       <div className="timelineContainer">
@@ -25,9 +47,67 @@ function Controls({
         <button className="playButton" onClick={() => handlePlayPause()}>
           {playBool ? <Pause /> : <Play />}
         </button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <div className="volumeContainer">
+          <button
+            className="muteButton"
+            onClick={() => {
+              volumeCheck(true);
+            }}
+          >
+            {muteBool ? (
+              <Mute />
+            ) : highVolumeBool ? (
+              <HighVolume />
+            ) : (
+              <LowVolume />
+            )}
+          </button>
+          <input
+            className="volumeSlider"
+            type="range"
+            min="0"
+            max="1"
+            step="any"
+            value={volumeValue}
+            onChange={(e) => setVolumeValue(e.target.value)}
+          ></input>
+        </div>
+        <div id="timeDisplay">
+          {Math.floor(currentTimeline / 60) > 0
+            ? Math.floor(currentTimeline / 60) + ":"
+            : "0:"}
+          {Math.floor(currentTimeline % 60) < 10 ? "0" : null}
+          {Math.floor(currentTimeline % 60)}
+          {" / "}
+          {Math.floor(videoDuration / 60) > 0
+            ? Math.floor(videoDuration / 60) + ":"
+            : "0:"}
+          {Math.floor(videoDuration % 60) < 10 ? "0" : null}
+          {Math.floor(videoDuration % 60)}
+        </div>
+        {!fullscreenBool && (
+          <button className="miniPlayerButton">
+            <MiniPlayer />
+          </button>
+        )}
+        {!fullscreenBool && (
+          <button
+            className="theaterButton"
+            onClick={() => {
+              theaterBool ? setTheaterBool(false) : setTheaterBool(true);
+            }}
+          >
+            {theaterBool ? <ExitTheater /> : <Theater />}
+          </button>
+        )}
+        <button
+          className="fullScreenButton"
+          onClick={() => {
+            fullscreenBool ? setFullscreenBool(false) : setFullscreenBool(true);
+          }}
+        >
+          {fullscreenBool ? <ExitFullscreen /> : <Fullscreen />}
+        </button>
       </div>
     </div>
   );
--- src/Routes/Watch.js
+++ src/Routes/Watch.js
@@ -1,13 +1,20 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Controls from "../Components/Controls";
 import video from "../TestVideo/testVideo.mp4";
 
 function Watch() {
   const videoRef = useRef(null);
-  const [videoElement, setVideoElement] = useState();
+  const [videoElement, setVideoElement] = useState(videoRef.current);
   const [playBool, setPlayBool] = useState(false);
   const [videoDuration, setVideoTotalDuration] = useState(0);
   const [currentTimeline, setCurrentTimeline] = useState(0);
+  const [theaterBool, setTheaterBool] = useState(false);
+  const [fullscreenBool, setFullscreenBool] = useState(false);
+  const [wasDefault, setWasDefault] = useState(true);
+  const [muteBool, setMuteBool] = useState(false);
+  const [highVolumeBool, setHighVolumeBool] = useState(true);
+  const [volumeValue, setVolumeValue] = useState(1);
+  const [volumeBeforeMute, setVolumeBeforeMute] = useState(0);
 
   //handles playing and pausing
 
@@ -19,8 +26,43 @@ function Watch() {
 
   const handleKeyDown = (e) => {
     const keyPress = e.key.toLowerCase();
-    if (keyPress === " " || keyPress === "k") {
-      handlePlayPause();
+    switch (keyPress) {
+      case " ":
+      case "k":
+        handlePlayPause();
+        break;
+      case "esc":
+      case "escape":
+        setFullscreenBool(false);
+        break;
+      case "f":
+        fullscreenBool ? setFullscreenBool(false) : setFullscreenBool(true);
+        break;
+      case "t":
+        if (fullscreenBool) {
+          setFullscreenBool(false);
+        }
+        theaterBool ? setTheaterBool(false) : setTheaterBool(true);
+        break;
+      case "m":
+        volumeCheck(true);
+        break;
+      case "arrowup":
+        const volumeUp = (Math.floor(volumeValue / 0.05) + 1) * 0.05;
+        if (volumeUp > 1) {
+          return;
+        }
+        setVolumeValue(volumeUp);
+        break;
+      case "arrowdown":
+        const volumeDown = (Math.floor(volumeValue / 0.05) - 1) * 0.05;
+        if (volumeDown < 0) {
+          return;
+        }
+        setVolumeValue(volumeDown);
+        break;
+      default:
+        return;
     }
   };
 
@@ -36,15 +78,88 @@ function Watch() {
     }
   };
 
-  //references video on page mount
+  //logic for keeping css in working order when entering or exiting fullscreen
+
+  /*
+   * Mini-player functionality will be added later when the home page is created
+   */
+
+  useEffect(() => {
+    if (fullscreenBool) {
+      if (theaterBool) {
+        setWasDefault(false);
+      } else {
+        setWasDefault(true);
+      }
+      setTheaterBool(false);
+      videoElement.requestFullscreen();
+    } else {
+      if (!wasDefault) {
+        setTheaterBool(true);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fullscreenBool]);
+
+  useLayoutEffect(() => {
+    document.onfullscreenchange = () => setFullscreenBool(false);
+    return () => {
+      document.onfullscreenchange = undefined;
+    };
+  });
+
+  //sets up reference to video on page mount
 
   useEffect(() => {
     setVideoElement(videoRef.current);
   }, []);
 
+  //Logic for volume
+
+  const volumeCheck = (wasMuteClicked) => {
+    if (wasMuteClicked) {
+      muteBool ? unmute() : mute();
+    } else {
+      if (volumeValue > 0.0001) {
+        setMuteBool(false);
+      }
+      if (muteBool) {
+        return;
+      }
+      volumeValue > 0.5 ? setHighVolumeBool(true) : setHighVolumeBool(false);
+      volumeValue == 0 ? slideToMute() : setMuteBool(false);
+      videoRef.current.volume = volumeValue;
+    }
+  };
+
+  const slideToMute = () => {
+    setMuteBool(true);
+    setVolumeBeforeMute(1);
+  };
+
+  const mute = () => {
+    setVolumeBeforeMute(volumeValue);
+    setMuteBool(true);
+    setVolumeValue(0);
+  };
+
+  const unmute = () => {
+    setMuteBool(false);
+    setVolumeValue(volumeBeforeMute);
+  };
+
+  useEffect(() => {
+    videoRef.current.muted = !videoRef.current.muted;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [muteBool]);
+
   return (
     <div id="watch" tabIndex={0} onKeyDown={handleKeyDown}>
-      <div className="videoContainer">
+      <div
+        className={`videoContainer ${theaterBool ? "theater" : ""} ${
+          fullscreenBool ? "fullscreen" : ""
+        }`}
+      >
         <video
           autoPlay
           ref={videoRef}
@@ -54,6 +169,9 @@ function Watch() {
           onTimeUpdate={handleTimeline}
           onPlay={() => setPlayBool(true)}
           onPause={() => setPlayBool(false)}
+          onDoubleClick={() =>
+            fullscreenBool ? setFullscreenBool(false) : setFullscreenBool(true)
+          }
         ></video>
         <Controls
           videoElement={videoElement}
@@ -62,6 +180,17 @@ function Watch() {
           handlePlayPause={handlePlayPause}
           videoDuration={videoDuration}
           currentTimeline={currentTimeline}
+          theaterBool={theaterBool}
+          setTheaterBool={setTheaterBool}
+          fullscreenBool={fullscreenBool}
+          setFullscreenBool={setFullscreenBool}
+          muteBool={muteBool}
+          setMuteBool={setMuteBool}
+          highVolumeBool={highVolumeBool}
+          setHighVolumeBool={setHighVolumeBool}
+          volumeValue={volumeValue}
+          setVolumeValue={setVolumeValue}
+          volumeCheck={volumeCheck}
         />
       </div>
     </div>
